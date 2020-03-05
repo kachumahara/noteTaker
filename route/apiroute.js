ba50882d9c4ba1b248af9352607d04908e003eef
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-05 10:56:27 -0700

    added files

--- route/apiroute.js
+++ route/apiroute.js
@@ -1,6 +1,16 @@
 var router = require("express").Router()
 var path = require("path")
-router.get("/notes", function(req, resp){
 
-})
+router.get("/notes", function(req, resp){
+path 
+.getNotes()
+.then(notes => resp.json(notes))
+.catch(err => resp.status(500).json(err));
+
+});
+router.post("/notes", (req, res) => {
+    path.addNote(re.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
 module.exports = router
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require("express")
 var html = require("./route/htmlroute")
 var api = require("./route/apiroute")
 var app = express()
+var path = require("path")
 
 var PORT = 3000 || process.env.PORT
 
-app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(express.static("public"))
-app.use("/api", api)
-app.use("/", html)
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
 app.listen(PORT, function(){
     console.log("listening on PORT: " + PORT)
