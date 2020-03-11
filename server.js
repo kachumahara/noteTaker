6b984036255761f1b744f331d1840d563d04c234
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-03-10 20:07:07 -0700

    corrected all errors

--- server.js
+++ server.js
@@ -5,72 +5,68 @@ var uuid = require("uuid").v4;
 var path = require("path");
 var fs = require("fs");
 var util = require("util");
+
 var PORT = process.env.PORT || 3000;
 
+var dataNotes = [];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-var dataNote = [];
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(express.static("public"))
-
-// api routes 
-app.get("/api/notes", function(req,res) {
-    fs.readFile(__dirname + "/db/db.json", "utf8", function(err, data) {
-        if (err) throw err;
-        var notes = JSON.parse(data);
-        res.json(notes);
-        
-    })
+// api routes
+app.get("/api/notes", function(req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf8", function(err, data) {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  })
 });
+// function that adds new note
+app.post("/api/notes", function(req, res) {
+  let newNote = req.body;
+  newNote.id = uuid();
 
-app.post("/api/notes", function(req,res) {
-    let newNote = req.body;
-    newNote.id = uuid();
-
-    fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => { 
-
-    
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     var notes = JSON.parse(data);
     notes.push(newNote);
-    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
-        if (err) throw err;
-        res.json(newNote);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), err => {
+      if (err) throw err;
+      res.json(newNote);
     })
-})
+  })
 });
 
-// function that returns a response of note not found
+// function that that deletes notes.
 
-app.delete("/api/notes/:id", function(req,res) {
-    var noteIdToDelete = req.params.id;
-    var noteDeleted = false;
-    fs.readFile(__dirname + "/db/db.json", "utf8", (err , data) => {
-        if (err) throw err;
-        var notes = JSON.parse(data);
-        for ( var i = 0; i < notes.length && !notesDeleted; i++) {
-            if (notes[i].id === noteIdToDelete) {
-            console.log(notes[i].id);
-            notes.splice(i, 1);
-            noteDeleted = true;
-        }
+app.delete("/api/notes/:id", function(req, res) {
+  var noteIdToDelete = req.params.id;
+  var noteDeleted = false;
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    for (var i = 0; i < notes.length && !noteDeleted; i++) {
+      if (notes[i].id === noteIdToDelete) {
+        console.log(notes[i].id);
+        notes.splice(i, 1);
+        noteDeleted = true;
+      }
     }
-    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
-        if (err) throw err;
-        res.json(notes);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), err => {
+      if (err) throw err;
+      res.json(notes);
     })
-    })
+  })
 });
+// route to index HTML file
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
-app.get("*", function(req,res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
-// require("./route/apiroute")(app);
-// require("./route/htmlroute")(app);
-
+// function that starts the server
 app.listen(PORT, function() {
-    console.log("listening on PORT: " + PORT)
-})
+  console.log("listening on PORT: " + PORT);
+});
